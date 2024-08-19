278e513b379a261bae9fd597b7bbf230e94ed5e6
Matt Mower <mowerm@gmail.com>
2024-08-19 15:45:52 +0000

    Add comments about non-named-imports to index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 import {compilerOptions} from './tsconfig.json' with {type: 'json'};
 console.log(compilerOptions);
+
+// Alternative that does not use named imports and builds+runs fine:
+/*
+import tsconfig from './tsconfig.json' with {type: 'json'};
+console.log(tsconfig.compilerOptions);
+*/
